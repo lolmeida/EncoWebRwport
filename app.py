dd8bd5c6e62e3c0f211fcc56e0f4eb9e4d946798
lolmeida <39884938+lolmeida@users.noreply.github.com>
2021-12-02 15:53:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -367,7 +367,7 @@ hide_st_style = """
             header {visibility: hidden}
             </style>
 """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+#st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # if __name__ == '__main__':
 # main()
